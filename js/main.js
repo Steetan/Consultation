30af3c6e3669e374cf422db138288174f5fa5537
Steetan <vlad.no.96@bk.ru>
2023-05-27 19:51:33 +0300

    proj fix

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ document.querySelectorAll(".questions__block-top").forEach(function(event) {
         event.classList.toggle("questions__block-top--active")
     })
 })
+
+document.querySelector(".btn--contacts").addEventListener("click", function(event) {
+    if(document.querySelector(".contacts__input--text").value == "" 
+    || document.querySelector(".contacts__input--tel").value == "") {
+        event.preventDefault()
+        alert("Не введены обязательные поля!")
+    }
+})
